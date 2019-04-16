55832f05a049c34e31a1070e4515d2992021ace1
Softwarica College Lab <softwarica@softwarica.com>
2019-04-16 11:59:57 +0545

    asd

---
+++ app/src/main/java/com/e/dictionaryapp/CapitalActivity.java
@@ -0,0 +1,44 @@
+package com.e.dictionaryapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CapitalActivity extends AppCompatActivity {
+
+    private TextView tvMeaning;
+    private Button btnback;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capital);
+
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle!=null){
+            String capital = bundle.getString("Capital");
+            tvMeaning=findViewById(R.id.tvMeaning);
+            tvMeaning.setText(capital);
+
+        }
+        else
+        {
+            Toast.makeText(CapitalActivity.this,"No capital!",Toast.LENGTH_LONG).show();
+        }
+
+        btnback= findViewById(R.id.btnback);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backintent = new Intent(CapitalActivity.this,MainActivity.class);
+                backintent.putExtra("back","You came back.");
+                startActivity(backintent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/e/dictionaryapp/MainActivity.java
+++ app/src/main/java/com/e/dictionaryapp/MainActivity.java
@@ -1,13 +1,82 @@
 package com.e.dictionaryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+        public static final String countries[]={
+                "Nepal","Kathmandu",
+                "India","New Delhi",
+                "China","Beijing",
+                "Bhutan","Dhaka",
+                "USA","Washington D.C.",
+                "UK","London",
+                "Canada","Ottawa"
+
+        };
+
+        private Map<String,String> dictionary;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView lstCountries = findViewById(R.id.lstCountries);
+
+        dictionary = new HashMap<>();
+        for (int i=0;i<countries.length;i+=2 ){
+            dictionary.put(countries[i],countries[i+1]);
+        }
+
+
+        ArrayAdapter myAdapter = new ArrayAdapter<>(
+                MainActivity.this, android.R.layout.simple_list_item_checked,new ArrayList<String>(dictionary.keySet())
+        );
+
+        lstCountries.setAdapter(myAdapter);
+
+        lstCountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String country= parent.getItemAtPosition(position).toString();
+                String capital = dictionary.get(country);
+
+                Intent intent = new Intent(MainActivity.this,CapitalActivity.class);
+                intent.putExtra("Capital",capital);
+                startActivity(intent);
+
+
+            }
+        });
+
+        Bundle bundle= getIntent().getExtras();
+        if (bundle!=null){
+            String back = bundle.getString("back");
+            Toast.makeText(MainActivity.this,back,Toast.LENGTH_LONG).show();
+
+
+
+
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this,"No capital!",Toast.LENGTH_LONG).show();
+        }
     }
 }
